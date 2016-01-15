6b92fa952932cc5f46bae1c371e6605f26c66653
Ryan Brodie <ryanbrodie@me.com>
2016-01-15 13:25:55 +0000

    Initial Commit

---
+++ IsContentBlockerEnabled/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IsContentBlockerEnabled
+//
+//  Created by Ryan Brodie on 15/01/2016.
+//  Copyright © 2016 Ryan Brodie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IsContentBlockerEnabled/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IsContentBlockerEnabled
+//
+//  Created by Ryan Brodie on 15/01/2016.
+//  Copyright © 2016 Ryan Brodie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
